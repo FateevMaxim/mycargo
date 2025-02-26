4e00d7bb591f74a6fbd256ef565be478ac95e65d
FateevMaxim <fateev.maxim@gmail.com>
2025-02-27 01:33:43 +0500

    import fix and new china

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,15 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
+import 'tw-elements';
+
+// Initialization for ES Users
+import {
+    Modal,
+    Ripple,
+    initTWE,
+} from "tw-elements";
+
+initTWE({ Modal, Ripple });
 
 window.Alpine = Alpine;
 
